bdac110d7b446c51a8145a56ac7469771b485463
JoMingyu <city7310@naver.com>
2018-11-25 18:34:42 +0900

    position ko en mapping, valid position names 추가

---
+++ metadata.py
@@ -0,0 +1,10 @@
+_POSITION_KO_EN_MAPPING = {
+    '탑': 'top',
+    '정글': 'jungle',
+    '미드': 'mid',
+    '원딜': 'adc',
+    '서폿': 'support'
+}
+
+_VALID_POSITION_NAMES = list(_POSITION_KO_EN_MAPPING.keys()) + list(_POSITION_KO_EN_MAPPING.values())
+
